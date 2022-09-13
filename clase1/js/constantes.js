8f2d0b850b54aa92fcba2c1c3a71e550562e4d37
Marcos <exegeses@gmail.com>
2022-09-12 21:04:57 -0300

    Práctica clase 1

---
+++ clase1/js/constantes.js
@@ -0,0 +1,11 @@
+let numero = 127;
+const nombre = 'Rick Sánchez';
+console.log(numero);
+console.log(nombre);
+/**
+ * más código
+ */
+
+numero = '127-c';
+nombre = 'Otro Nombre';
+console.log(numero);
