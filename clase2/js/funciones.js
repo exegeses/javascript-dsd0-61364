5b6685bbc9003cca2f30a111f8cdc0a1aeca1b77
Marcos <exegeses@gmail.com>
2022-09-21 18:39:20 -0300

    metarial clase 2

---
+++ clase2/js/funciones.js
@@ -0,0 +1,8 @@
+//ubicamos elemento dentro del DOM
+const caja = document.getElementById('caja');
+
+//creamos funciones de control
+function pintarVerde()
+{
+    caja.style.backgroundColor = '#0a3';
+}
