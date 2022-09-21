105ecb366d6b47e32e060f5eeaffe14f8a35f5f0
Marcos <exegeses@gmail.com>
2022-09-21 20:30:40 -0300

    funciones con parátros + coloPicker

---
+++ clase3/js/parametros.js
@@ -0,0 +1,16 @@
+//ubicamos elemento dentro del DOM
+const caja = document.getElementById('caja');
+const colorPicker = document.getElementById('colorPicker');
+
+
+//declaramos función de control
+function pintar( codigoColor )
+{
+    caja.style.backgroundColor =  codigoColor;
+}
+
+function pintarInput()
+{
+    let codigoColor = colorPicker.value;
+    caja.style.backgroundColor =  codigoColor;
+}
